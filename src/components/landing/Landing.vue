d5f1fdb8860e827bad6e260505b0a20a58f7fb25
Olivia Lonsdale <932400926@acgedu.com>
2020-02-28 17:27:47 +1300

    livs first

---
+++ src/components/landing/Landing.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <router-link v-bind:to="'/Details'">Liv</router-link>
+      <div v-for="Detail in Details" :key="Detail.id">
+        
+        <div>
+          <router-link :to="{
+            name: 'detail',
+            params: { DetailId: Detail.id }
+          }">
+          <h2>{{ Detail.title }}</h2>
+          </router-link>
+          <span>
+              <router-link :to="{
+            name: 'Detail-edit',
+            params: { DetailId: Detail.id }
+          }">
+     Edit
+          </router-link>
+          </span>
+        </div>
+      </div>
+</div>
+
+</template>
