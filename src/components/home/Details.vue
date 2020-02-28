c759b90c5c3f65856139c9f6267e35870c88ccaa
Amber louise Page <932400489@acgedu.com>
2020-02-28 17:24:14 +1300

    ambers first

---
+++ src/components/home/Details.vue
@@ -0,0 +1,22 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "Details",
+    data: function() {
+        return {
+            Details: [{
+                id: "1",
+                info: "theujwikahdfuewhfefbdsiufkhgbesudjkfgbcvewiuksdgjfbiuewjkgsdfuisejkgfuuug",
+            },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
