615767c874129a54d391f3d9be2ea5d43330ff4d
Abby Cloete <932400748@acgedu.com>
2020-02-28 16:04:30 +1300

    file changes

---
+++ src/components/home/Projects.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="projects-list"></div>
+  
+</template>
+
+<script>
+
+export default {
+name: "Projects",
+data: function(){
+    return {
+        project: [
+            {
+                id: "1",
+                title: "",
+                details: "",
+                image: ""
+            },
+            {
+                id: "2",
+                title: "",
+                details: "",
+                image: ""
+            },
+            {
+                id: "3",
+                title: "",
+                details: "",
+                image: ""
+            },
+        ]
+    }
+}
+}
+
+
+
+</script>
+
+<style>
+
+</style>
